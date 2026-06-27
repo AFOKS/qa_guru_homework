74964ca32753194f83a22e6222711bf6d7718e56
AFOKS <azhnov00@mail.ru>
2026-06-27 13:10:06 +0300

    add main.ru.py

---
+++ main.ru.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+
+if a!=b and b!=c:
+    print('Разносторонний')
+elif a == b == c:
+    print('Равносторонний')
+else:
+    print('Равнобедренный')
