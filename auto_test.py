9421234014f415160b7e5bd051c36643f7e048ec
AFOKS <azhnov00@mail.ru>
2026-06-29 15:14:09 +0300

    Test commit pc

--- auto_test.py
+++ auto_test.py
@@ -2,9 +2,10 @@ from selene import browser, be, have, by
 
 
 browser.open('https://google.com')
-if browser.element(by.text('Accept all')).matching(be.visible):
-   browser.element(by.text('Accept all')).click()
+#if browser.element(by.text('Accept all')).matching(be.visible):
+   #browser.element(by.text('Accept all')).click()
 browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
 browser.element('html').should(have.text('About this page'))
 
-# browser.element('[id="search"]').should(have.text('QA.GURU: Курсы тестировщиков'))
+
+#browser.element('[id="search"]').should(have.text('QA.GURU: Курсы тестировщиков'))
