3c91ab015b9f7840c52646410bd8501aea1e03b2
AFOKS <azhnov00@mail.ru>
2026-06-29 15:05:28 +0300

    New code and branch. Test pc with tokens github

---
+++ auto_test.py
@@ -0,0 +1,10 @@
+from selene import browser, be, have, by
+
+
+browser.open('https://google.com')
+if browser.element(by.text('Accept all')).matching(be.visible):
+   browser.element(by.text('Accept all')).click()
+browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
+browser.element('html').should(have.text('About this page'))
+
+# browser.element('[id="search"]').should(have.text('QA.GURU: Курсы тестировщиков'))
