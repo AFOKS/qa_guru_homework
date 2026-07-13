71126bfbccc97e592b1df5e0ce1b4bfd9fce3c12
AFOKS <azhnov00@mail.ru>
2026-07-13 15:52:03 +0300

    fixed the test and added new scenarios.

--- lesson_4/automation_practice_form/automation_practice_form_test_suite.py
+++ lesson_4/automation_practice_form/automation_practice_form_test_suite.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import automation_practice_form_po
 
 class AutomationPracticeFormTestSuite:
@@ -18,11 +20,24 @@ class AutomationPracticeFormTestSuite:
     def test_form_positive01(self):
         self.automation_practice_form.fill_in_form(self.tmp_file_name, "Dmitry","Bugaev", "bugaev@example.com", "Male","1234567890",("1988", "4", "22"),("Maths", "English"), ("Sports", "Music"), "г. Санкт-Петербург, ул. Невский проспект, д 101", "NCR",  "Noida")
         self.automation_practice_form.assert_form(self.tmp_file_name, "Dmitry","Bugaev", "bugaev@example.com", "Male","1234567890",("1988", "4", "22"),("Maths", "English"), ("Sports", "Music"), "г. Санкт-Петербург, ул. Невский проспект, д 101", "NCR",  "Noida")
-
+    # Изменил данные теста
     def test_form_positive02(self):
-        #  придумай другой набор тестовых данных
-        self.automation_practice_form.fill_in_form(self.tmp_file_name, "Dmitry","Bugaev", "bugaev@example.com", "Male","1234567890",("1988", "4", "22"),("Maths", "English"), ("Sports", "Music"), "г. Санкт-Петербург, ул. Невский проспект, д 101", "NCR",  "Noida")
-        self.automation_practice_form.assert_form(self.tmp_file_name, "Dmitry","Bugaev", "bugaev@example.com", "Male","1234567890",("1988", "4", "22"),("Maths", "English"), ("Sports", "Music"), "г. Санкт-Петербург, ул. Невский проспект, д 101", "NCR",  "Noida")
+        self.automation_practice_form.fill_in_form(self.tmp_file_name, "Anna","Fedotova", "fed@as.ru", "Female","8005553555",("2000", "10", "01"),("Biology", "Civics"), ("Reading", "Music"), "г. Самосир, ул. Есенина, д 123", "Haryana",  "Panipat")
+        self.automation_practice_form.assert_form(self.tmp_file_name, "Anna","Fedotova", "fed@as.ru", "Female","8005553555",("2000", "10", "01"),("Biology", "Civics"), ("Reading", "Music"), "г. Самосир, ул. Есенина, д 123", "Haryana",  "Panipat")
+
+    def test_form_positive03(self):
+        self.automation_practice_form.fill_in_form(self.tmp_file_name, "Криштиано","Рональндо", "cr7@test.com", "Male","8777777701",("1985", "2", "05"),("English", "Biology", "Economics", "Arts"), ("Sports", "Reading", "Music"), "г. Лиссабон, ул. Рональдо, д 7", "Uttar Pradesh",  "Merrut")
+        self.automation_practice_form.assert_form(self.tmp_file_name, "Криштиано","Рональндо", "cr7@test.com", "Male","8777777701",("1985", "2", "05"),("English", "Biology", "Economics", "Arts"), ("Sports", "Reading", "Music"), "г. Лиссабон, ул. Рональдо, д 7", "Uttar Pradesh",  "Merrut")
+
+    def test_form_positive04(self):
+        self.automation_practice_form.fill_in_form(self.tmp_file_name, "Человек","Паук", "spiderman@hero.com", "Other","8567348990",("2005", "10", "01"),("Physics", "Biology", "Computer Science", "Arts",), ("Sports", "Reading", "Music"), "г. Нью-йорк, Бруклин", "NCR",  "Delhi")
+        self.automation_practice_form.assert_form(self.tmp_file_name, "Человек","Паук", "spiderman@hero.com", "Other","8567348990",("2005", "10", "01"),("Physics", "Biology", "Computer Science", "Arts"), ("Sports", "Reading", "Music"), "г. Нью-йорк, Бруклин", "NCR",  "Delhi")
+
+    def test_form_positive05(self):
+        self.automation_practice_form.fill_in_form(self.tmp_file_name, "IRON","ЧЕЛОВЕК", "ironman@hero.ru", "Male","9999999999",("1990", "4", "07"),("Maths", "Physics", "Biology", "Computer Science", "Arts", "Economics"), ("Sports", "Music"), "г. Moscow, Вашингтон street", "Uttar Pradesh",  "Agra")
+        self.automation_practice_form.assert_form(self.tmp_file_name, "IRON","ЧЕЛОВЕК", "ironman@hero.ru", "Male","9999999999",("1990", "4", "07"),("Maths", "Physics", "Biology", "Computer Science", "Arts", "Economics"), ("Sports", "Music"), "г. Moscow, Вашингтон street", "Uttar Pradesh",  "Agra")
+
+
 
     def tear_down(self):
         if os.path.exists(self.tmp_file_name):
@@ -32,15 +47,42 @@ class AutomationPracticeFormTestSuite:
 
 test_suite = AutomationPracticeFormTestSuite()
 
-try:
-    test_suite.setup()
-    test_suite.test_form_positive01()
+# Запустил тесты в правильной последовательности
 
-    #  в общем виде работать не будет,
-    #  и в нашем виде работать не будет, так как setup и tear_down должны вызываться перед каждым тестом ...
-    #  так же в идеале перед каждым тестом создается PO -
-    #  подумайте как решить малой кровью (в будущем через Test Runner)
+test_suite.setup() #создает каждый раз файл для прикрепеления
+try:
+    test_suite.test_form_positive01()
+finally:
+    test_suite.tear_down()
+    print("\n✅ Тест №1 успешно пройден!!!")
+
+
+test_suite.setup()
+try:
     test_suite.test_form_positive02()
 finally:
     test_suite.tear_down()
+    print("\n✅ Тест №2 успешно пройден!!!")
 
+test_suite.setup()
+try:
+    test_suite.test_form_positive03()
+finally:
+    test_suite.tear_down()
+    print("\n✅ Тест №3 успешно пройден!!!")
+
+test_suite.setup()
+try:
+    test_suite.test_form_positive04()
+    time.sleep(5)
+finally:
+    test_suite.tear_down()
+    print("\n✅ Тест №4 успешно пройден!!!")
+
+test_suite.setup()
+try:
+    test_suite.test_form_positive05()
+    time.sleep(5)
+finally:
+    test_suite.tear_down()
+    print("\n✅ Тест №5 успешно пройден!!!")
