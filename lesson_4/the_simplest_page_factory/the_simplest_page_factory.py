f6c74f37e3bebed3ee20b8144b5b7e5c9ef580eb
AFOKS <azhnov00@mail.ru>
2026-07-16 11:50:47 +0300

    add new test-case

--- lesson_4/the_simplest_page_factory/the_simplest_page_factory.py
+++ lesson_4/the_simplest_page_factory/the_simplest_page_factory.py
@@ -2,60 +2,103 @@ import time
 from selenium import webdriver
 from seleniumpagefactory.Pagefactory import PageFactory
 
-# Создал тест но авторизацию
-# 1. Описываем класс страницы, наследуясь от PageFactory
+
 class LoginPage(PageFactory):
+
     def __init__(self, driver):
         self.driver = driver
-        # Локаторы задаются в виде словаря. Ключ станет именем переменной-элемента.
+
         self.locators = {
-            "username_input": ('name', 'email'),
-            "password_input": ('name', 'password'),
+            "username_input": ("name", "email"),
+            "password_input": ("name", "password"),
             "login_button": ("CSS", "button[data-qa='login-button']"),
-            "signup_name": ('name', 'name'),
-            "signup_email": ('name', 'email'),
+
+            "signup_name": ("name", "name"),
+            "signup_email": ("CSS", "input[data-qa='signup-email']"),
             "signup_button": ("CSS", "button[data-qa='signup-button']")
         }
 
-    # Метод, использующий возможности Page Factory
     def login(self, email, password):
-        # Элементы инициализируются «на лету» и имеют расширенные методы
-        self.username_input.set_text(email)  # Ввод текста + очистка
+        self.username_input.set_text(email)
         self.password_input.set_text(password)
-
 
     def click_login(self):
         self.login_button.click()
-    def signup(self, name, email,):
+
+    def signup(self, name, email):
         self.signup_name.set_text(name)
         self.signup_email.set_text(email)
-# 2. Основной скрипт теста
-if __name__ == "__main__":
-    driver = webdriver.Chrome()
-    driver.get("https://www.automationexercise.com/login")
-    
-    # Инициализация страницы
-    login_page = LoginPage(driver)
-    
-    # Вызов логики
-    login_page.login("zxc@test.com", "123456")
-    login_page.click_login()
-    
-    time.sleep(5)
-    driver.quit()
-    print("Тест на аторизацию успешно пройден!!")
+
+    def click_signup(self):
+        self.signup_button.click()
+
+
+class TestSuite:
+
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://www.automationexercise.com/login")
+        self.page = LoginPage(self.driver)
+
+    def teardown(self):
+        time.sleep(3)
+        self.driver.quit()
+
+    # Тест 1. Успешная авторизация
+    def test_login_success(self):
+        self.setup()
+
+        self.page.login("zxc@test.com", "123456")
+        self.page.click_login()
+
+        print("✅ test_login_success выполнен")
+
+        self.teardown()
+
+    # Тест 2. Авторизация с неверным паролем
+    def test_login_wrong_password(self):
+        self.setup()
+
+        self.page.login("zxc@test.com", "111111")
+        self.page.click_login()
+
+        print("✅ test_login_wrong_password выполнен")
+
+        self.teardown()
+
+    # Тест 3. Авторизация с пустыми полями
+    def test_login_empty_fields(self):
+        self.setup()
+
+        self.page.login("", "")
+        self.page.click_login()
+
+        print("✅ test_login_empty_fields выполнен")
+
+        self.teardown()
+
+    # Тест 4. Переход к регистрации
+    def test_signup(self):
+        self.setup()
+
+        self.page.signup(
+            "Andrey",
+            "qabest@test.com"
+        )
+        self.page.click_signup()
+
+        print("✅ test_signup выполнен")
+
+        self.teardown()
+
 
 if __name__ == "__main__":
-    driver = webdriver.Chrome()
-    driver.get("https://www.automationexercise.com/login")
 
-    # Инициализация страницы
-    signup_page = SignupPage(driver)
+    tests = TestSuite()
 
-    # Вызов логики
-    signup_page.login("zxcc@test.com", "1234567")
-    signup_page.click_login()
+    tests.test_login_success()
+    tests.test_login_wrong_password()
+    tests.test_login_empty_fields()
+    tests.test_signup()
 
-    time.sleep(5)
-    driver.quit()
-    print("Тест на регситрацию успешно пройден!!")
