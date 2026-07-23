ee143268e4e67b835a41fbf4bca332559c6f04e1
AFOKS <azhnov00@mail.ru>
2026-07-23 12:29:02 +0300

    Написал тест для "API", который через indirect фикстуру меняет заголовки авторизации (User, Admin, Guest).

---
+++ lesson_6_pytest_2_1/pytest_param_example_02/test_authorization_with_token.py
@@ -0,0 +1,29 @@
+import pytest
+
+@pytest.fixture
+def auth_headers(request):
+    role = request.param
+
+    headers = {
+        "User": {
+            "Authorization": "Bearer user_token"
+        },
+        "Admin": {
+            "Authorization": "Bearer admin_token"
+        },
+        "Guest": {
+            "Authorization": "Bearer guest_token"
+        }
+    }
+
+    return headers[role]
+
+@pytest.mark.parametrize(
+    "auth_headers",
+    ["User", "Admin", "Guest"],
+    indirect=True
+)
+def test_api_authorization(auth_headers):
+    print(f"-> Заголовки запроса: {auth_headers}")
+
+    assert "Authorization" in auth_headers
