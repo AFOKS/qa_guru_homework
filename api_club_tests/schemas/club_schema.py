1640d10498f1b38960d1e094054a34ad296d684c
AFOKS <azhnov00@mail.ru>
2026-09-07 10:59:17 +0300

    add new schema

--- api_club_tests/schemas/club_schema.py
+++ api_club_tests/schemas/club_schema.py
@@ -53,3 +53,113 @@ success_create_club = {
     "modified"
   ]
 }
+get_club_schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "Get club response",
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "number"
+        },
+        "bookTitle": {
+            "type": "string"
+        },
+        "bookAuthors": {
+            "type": "string"
+        },
+        "publicationYear": {
+            "type": "number"
+        },
+        "description": {
+            "type": "string"
+        },
+        "telegramChatLink": {
+            "type": "string"
+        },
+        "owner": {
+            "type": "number"
+        },
+        "members": {
+            "type": "array",
+            "items": {
+                "type": "number"
+            }
+        },
+        "reviews": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "number"
+                    },
+                    "club": {
+                        "type": "number"
+                    },
+                    "user": {
+                        "type": "object",
+                        "properties": {
+                            "id": {
+                                "type": "number"
+                            },
+                            "username": {
+                                "type": "string"
+                            }
+                        },
+                        "required": [
+                            "id",
+                            "username"
+                        ]
+                    },
+                    "review": {
+                        "type": "string"
+                    },
+                    "assessment": {
+                        "type": "number"
+                    },
+                    "readPages": {
+                        "type": "number"
+                    },
+                    "created": {
+                        "type": "string"
+                    },
+                    "modified": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "id",
+                    "club",
+                    "user",
+                    "review",
+                    "assessment",
+                    "readPages",
+                    "created",
+                    "modified"
+                ]
+            }
+        },
+        "created": {
+            "type": "string"
+        },
+        "modified": {
+            "type": [
+                "string",
+                "null"
+            ]
+        }
+    },
+    "required": [
+        "id",
+        "bookTitle",
+        "bookAuthors",
+        "publicationYear",
+        "description",
+        "telegramChatLink",
+        "owner",
+        "members",
+        "reviews",
+        "created",
+        "modified"
+    ]
+}
